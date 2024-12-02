2b1ad8468829a588947f94f4c8f278fc19470f7c
beginner <121371582+LSH160981@users.noreply.github.com>
2024-12-02 20:16:21 +0800

    Update grass.sh

--- grass.sh
+++ grass.sh
@@ -10,15 +10,21 @@ fi
 # 安装必要的依赖
 function install_dependencies() {
     echo "安装必要的依赖..."
-    apt update && apt upgrade -y
-    apt install curl wget git -y
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install curl wget git -y
 }
 
-# 安装 Python 3.11 
+# 安装 Python
 function install_python() {
-    echo "安装 Python 3.11..."
+    # 判断一下是否已经安装了 Python
+    if command -v python3.11 >/dev/null; then
+        echo "Python 已经安装，跳过安装步骤。"
+        return
+    fi
+
+    echo "安装 Python 3 ..."
     add-apt-repository ppa:deadsnakes/ppa -y
-    apt install python3.11 python3.11-venv python3.11-dev python3-pip -y
+    sudo apt install python3.11 python3.11-venv python3.11-dev python3-pip -y
 
     echo "验证 Python 版本..."
     python3.11 --version
@@ -32,6 +38,12 @@ git clone https://github.com/LSH160981/grass-code
 
 # 进入代码目录
 cd grass-code
+
+# 创建虚拟环境
+python -m venv grass
+
+# 激活虚拟环境
+source grass/bin/activate
 
 # 安装依赖
 python -m pip install -r requirements.txt
