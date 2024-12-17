a840ea5b90f3c576c2904bb63276dfabc5e38d7d
beginner <121371582+LSH160981@users.noreply.github.com>
2024-12-17 15:33:44 +0800

    Update grass.sh

--- grass.sh
+++ grass.sh
@@ -2,9 +2,6 @@
 
 # 安装必要的依赖
 function install_dependencies() {
-    echo "安装必要的依赖..."
-    sudo apt update && sudo apt upgrade -y
-    sudo apt install curl wget git -y
 }
 
 # 安装 Python
@@ -16,7 +13,7 @@ function install_python() {
     fi
 
     echo "安装 Python 3 ..."
-    add-apt-repository ppa:deadsnakes/ppa -y
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
     sudo apt install python3.11 python3.11-venv python3.11-dev python3-pip -y
 
     echo "验证 Python 版本..."
@@ -27,20 +24,22 @@ install_dependencies
 install_python
 
 # 下载GitHub的代码
-git clone https://github.com/LSH160981/grass-code
+sudo git clone https://github.com/LSH160981/grass-code
 
 # 进入代码目录
 cd grass-code
 
 # 创建虚拟环境
-python3 -m venv grass
+sudo python3 -m venv grass
+
+# 修改虚拟环境目录权限，以便当前用户可以使用
+sudo chown -R $USER:$USER grass
 
 # 激活虚拟环境
 source grass/bin/activate
 
 # 安装依赖
-python3 -m pip install -r requirements.txt
+sudo python3 -m pip install -r requirements.txt
 
 # 在后台运行脚本
-nohup python3 main.py > output.log 2>&1 &
-
+nohup sudo python3 main.py > output.log 2>&1 &
