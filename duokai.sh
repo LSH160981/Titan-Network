50b7572fe95b51c03266b2e614997e4f7926c45b
beginner <121371582+LSH160981@users.noreply.github.com>
2025-02-21 09:02:40 +0800

    Update duokai.sh

--- duokai.sh
+++ duokai.sh
@@ -8,7 +8,7 @@ id="B9AF3196-E4E4-4A50-BDE3-8F0C3B882428"
 container_count=5
 start_rpc_port=30000
 custom_storage_path=""
-storage_gb=21
+storage_gb=5
 
 # 解析命令行参数
 while getopts "i:c:p:s:g:" opt; do
