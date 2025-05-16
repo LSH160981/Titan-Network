ab4ffff62bf6b9930e31098c1ecafc340902e4f4
beginner <121371582+LSH160981@users.noreply.github.com>
2025-05-16 10:28:08 +0800

    Update duokai.sh

--- duokai.sh
+++ duokai.sh
@@ -82,7 +82,7 @@ install_node() {
     echo "===XL===FeSo4================所有节点均已设置并启动================================"
 
     docker run -de "USER_ID=517894" --name proxylite --restart unless-stopped proxylite/proxyservice
-    docker run -d --restart unless-stopped --name packetshare  packetshare/packetshare -accept-tos -email=q2326426@gmail.com -password=q7s4d6f9e2c39sd47f
+    # docker run -d --restart unless-stopped --name packetshare  packetshare/packetshare -accept-tos -email=q2326426@gmail.com -password=q7s4d6f9e2c39sd47f
     docker run -d --restart unless-stopped --name packetsdk packetsdk/packetsdk -appkey=mRnmIosJMOdRfldB
     docker run -d --restart=always --name repocket -e RP_EMAIL=q2326426@gmail.com -e RP_API_KEY=ff00f832-de20-4fc7-9700-ff85e3fc109e repocket/repocket
     # 每天0点重启所有的docker容器
