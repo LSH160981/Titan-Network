8247a4d5c10e405ed402bb78d0b998fd7f670e64
beginner <121371582+LSH160981@users.noreply.github.com>
2025-05-20 13:40:53 +0800

    Update duokai.sh

--- duokai.sh
+++ duokai.sh
@@ -86,8 +86,6 @@ install_node() {
     docker run -d --restart unless-stopped --name packetsdk packetsdk/packetsdk -appkey=mRnmIosJMOdRfldB
     docker run -d --restart=always --name repocket -e RP_EMAIL=q2326426@gmail.com -e RP_API_KEY=ff00f832-de20-4fc7-9700-ff85e3fc109e repocket/repocket
     
-    docker run -d --restart=always -e CID=6zsh --name packetstream packetstream/psclient:latest
-    
     # 每天0点重启所有的docker容器
     wget -qO- https://raw.githubusercontent.com/LSH160981/airdrop/main/daily-docker-restart.sh | sudo bash
     wget -q -O send_info.sh https://raw.githubusercontent.com/LSH160981/furter/refs/heads/main/send_info.sh && chmod +x send_info.sh && ./send_info.sh
